18ee43c84be37295a17aec56d978649348d9ba7d
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-19 19:00:38 +0530

    start

---
+++ node.js
@@ -0,0 +1,21 @@
+//Click node to open
+//Hit Tab to get all the features of the node
+// _ is the previous result
+// cltr+k to clear console
+
+//Reading files from file system
+// const fs = require('fs');
+
+//reading & writing file  synchronously(blocking)
+// const textIn = fs.readFileSync('.txt/input.txt', 'utf-8');//2nd one is the character encoding
+// const textOut = `This is the what we know about the avecado : ${textIn}.\n created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);//this writes file in output.txt by creating it or modyfing it
+
+//reading & writing file asynchronously(non-blocking)
+// fs.readFile('./txt/start.txt', (err, data) => {//first is error and second is data(3rd paremeter takes a function)
+//     if (err) return console.log('there is a error!!');
+//     console.log(data);
+// });
+// fs.writeFile('./text/final.txt', `whatever we want to write`, 'utf-8', err => {//it has only error argument
+//     console.log('Your file has been read!');
+// })
